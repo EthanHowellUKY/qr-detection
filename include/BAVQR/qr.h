f26c8988aa5e95b8fa5c78c8bd3eba4ddb516ea8
Ethan Howell <ehowell@bastiansolutions.com>
2021-05-09 19:52:58 -0400

    Converted to decoding with zbar to accurately identify qr boundaries in image recognition.

--- include/BAVQR/qr.h
+++ include/BAVQR/qr.h
@@ -9,10 +9,13 @@
 #include <opencv2/core.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
+#include "opencv2/imgproc.hpp"
+#include <zbar.h>
 #include <qrencode.h>
 #include <png.h>
 #include <string>
 #include <iostream>
+#include <vector>
 
 #define INCHES_PER_METER (100.0/2.54)
 
@@ -32,7 +35,7 @@ public:
     // -------------------------------- // 
 	//         PUBLIC FUNCTIONS         //
 	// -------------------------------- //
-    std::string decode_qr(const cv::Mat &to_decode);
+    std::string decode_qr(cv::Mat &to_decode);
     std::string decode_qr(const std::string &img);
     void read_qr();
     void encode_qr(const std::string &to_encode);
@@ -53,12 +56,20 @@ private:
     void do_exit(const std::string &msg, const int &code);
     void open_png(const std::string &file_name);
     void close_png();
+    void decode(cv::Mat &im);
+    void display(cv::Mat &im);
 
     // -------------------------------- // 
 	//        PRIVATE VARIABLES         //
 	// -------------------------------- //
+    typedef struct
+    {
+        std::string type;
+        std::string data;
+        std::vector<cv::Point> location;
+    } DecodedObject;
     
-    // File operation objects
+    // File operation objects//
     FILE *m_fp;
     png_structp m_png_ptr = NULL;
     png_infop m_info_ptr = NULL;
@@ -66,6 +77,7 @@ private:
     // QR Objects
     QRcode *m_qr;
     cv::QRCodeDetector m_decoder;
+    zbar::ImageScanner m_scanner;
 
     // MISC Members
     unsigned char *m_row, *m_q, *m_p;
@@ -77,12 +89,12 @@ private:
     int m_camera = 0; // Rear camera
     int m_api_id = cv::CAP_ANY;
 
-    std::string m_decoded;
-    std::string m_toencode;
-    std::string m_filename;
-    bool        m_qr_isnull = true;
+    DecodedObject   m_decoded;
+    std::string     m_toencode;
+    std::string     m_filename;
+    bool            m_qr_isnull = true;
 
-    double m_delay = 31.25; // 32 fps
+    double          m_delay = 31.25; // 32 fps
 };
 
 #endif // BAVQR_H
--- src/qr.cpp
+++ src/qr.cpp
@@ -1,13 +1,13 @@
 
 #include "BAVQR/qr.h"
 
-// -------------------------------- // 
+// -------------------------------- //
 //        PRIVATE FUNCTIONS         //
 // -------------------------------- //
 void QR::write_margin()
 {
     memset(m_row, 0xff, (m_real_width + 7) / 8);
-    for (int ii=0; ii<m_margin*m_size; ii++)
+    for (int ii = 0; ii < m_margin * m_size; ii++)
     {
         png_write_row(m_png_ptr, m_row);
     }
@@ -33,7 +33,11 @@ void QR::open_png(const std::string &file_name)
     if (m_info_ptr == NULL) { do_exit("Could not initialize info_ptr", -3); }
 
     // Exception handler
-    if (setjmp(png_jmpbuf(m_png_ptr))) { png_destroy_write_struct(&m_png_ptr, &m_info_ptr); do_exit("Could not set up Exception handling", 1); }
+    if (setjmp(png_jmpbuf(m_png_ptr)))
+    {
+        png_destroy_write_struct(&m_png_ptr, &m_info_ptr);
+        do_exit("Could not set up Exception handling", 1);
+    }
 
     m_fp = fopen(file_name.c_str(), "wb");
     if (m_fp == NULL) { do_exit("Could not open file: " + file_name, -4); }
@@ -42,13 +46,13 @@ void QR::open_png(const std::string &file_name)
 
     // Write header (8 bit colour depth)
     png_set_IHDR(m_png_ptr, m_info_ptr, m_real_width, m_real_width,
-            1, PNG_COLOR_TYPE_GRAY, PNG_INTERLACE_NONE,
-            PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+                 1, PNG_COLOR_TYPE_GRAY, PNG_INTERLACE_NONE,
+                 PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
 
     png_set_pHYs(m_png_ptr, m_info_ptr,
-        m_dpi * INCHES_PER_METER,
-        m_dpi * INCHES_PER_METER,
-        PNG_RESOLUTION_METER);
+                 m_dpi * INCHES_PER_METER,
+                 m_dpi * INCHES_PER_METER,
+                 PNG_RESOLUTION_METER);
 
     png_write_info(m_png_ptr, m_info_ptr);
 }
@@ -62,23 +66,80 @@ void QR::close_png()
     }
     if (m_fp != NULL) { fclose(m_fp); }
     if (m_row != NULL) { free(m_row); }
-    if (!m_qr_isnull) { QRcode_free(m_qr); m_qr_isnull = true; }
+    if (!m_qr_isnull)
+    {
+        QRcode_free(m_qr);
+        m_qr_isnull = true;
+    }
 }
 
-// -------------------------------- // 
+void QR::decode(cv::Mat &im)
+{
+    // Convert image to grayscale
+    cv::Mat imGray;
+    cv::cvtColor(im, imGray, cv::COLOR_BGRA2GRAY);
+
+    // Wrap image data in a zbar image
+    zbar::Image image(im.cols, im.rows, "Y800", (uchar *)imGray.data, im.cols * im.rows);
+
+    // Scan the image for barcodes and QRCodes
+    if (image) {
+        int n = m_scanner.scan(image);
+        // Print results
+        for (zbar::Image::SymbolIterator symbol = image.symbol_begin(); symbol != image.symbol_end(); ++symbol)
+        {
+            m_decoded.type = symbol->get_type_name();
+            m_decoded.data = symbol->get_data();
+
+            // Obtain location
+            for (int i = 0; i < symbol->get_location_size(); i++)
+            {
+                m_decoded.location.push_back(cv::Point(symbol->get_location_x(i), symbol->get_location_y(i)));
+            }
+        }
+    }
+}
+
+void QR::display(cv::Mat &im)
+{
+    std::vector<cv::Point> points = m_decoded.location;
+    std::vector<cv::Point> hull;
+
+    // If the points do not form a quad, find convex hull
+    if (points.size() > 4)
+        cv::convexHull(points, hull);
+    else
+        hull = points;
+
+    // Number of points in the convex hull
+    int n = hull.size();
+
+    for (int j = 0; j < n; j++)
+    {
+        cv::line(im, hull[j], hull[(j + 1) % n], cv::Scalar(255, 0, 0), 3);
+    }
+
+    // Display results
+    cv::imshow("Results", im);
+}
+
+// -------------------------------- //
 //           CONSTRUCTORS           //
 // -------------------------------- //
-QR::QR() {}
+QR::QR()
+{
+    m_scanner.set_config(zbar::ZBAR_QRCODE, zbar::ZBAR_CFG_ENABLE, 1);
+}
 
-// -------------------------------- // 
+// -------------------------------- //
 //            DESTRUCTOR            //
 // -------------------------------- //
-QR::~QR() { }
+QR::~QR() {}
 
-// -------------------------------- // 
+// -------------------------------- //
 //         PUBLIC FUNCTIONS         //
 // -------------------------------- //
-std::string QR::decode_qr(const cv::Mat &to_decode) { return m_decoder.detectAndDecode(to_decode); }
+std::string QR::decode_qr(cv::Mat &to_decode) { decode(to_decode); return m_decoded.data; }
 std::string QR::decode_qr(const std::string &img)
 {
     cv::Mat input_image = cv::imread(img);
@@ -89,20 +150,22 @@ void QR::read_qr()
 {
     cv::Mat frame;
     cv::VideoCapture cap;
+    
     cap.open(m_camera, m_api_id);
     if (!cap.isOpened())
     {
         std::cout << "Unable to open camera" << '\n';
-        m_decoded = "-1";
+        m_decoded.data = "-1";
     }
     else
     {
         while (true)
         {
             cap >> frame;
-            m_decoded = decode_qr(frame);
-            cv::imshow("Live", frame);
-            if (m_decoded != "") { break; }
+            decode(frame);
+            display(frame);
+            m_decoded.location.clear();
+            if (m_decoded.data.length()) { break; }
             if (cv::waitKey(m_delay) >= 0) { break; }
         }
     }
@@ -126,28 +189,28 @@ void QR::create_png(const std::string &file_name)
     write_margin();
 
     m_p = m_qr->data;
-    for (int ii=0; ii < m_qr->width; ii++)
+    for (int ii = 0; ii < m_qr->width; ii++)
     {
         m_bit = 7;
         memset(m_row, 0xff, (m_real_width + 7) / 8);
         m_q = m_row;
-        m_q += m_margin*m_size / 8;
-        m_bit = 7 - (m_margin*m_size % 8);
-        for (int jj=0; jj < m_qr->width; jj++)
+        m_q += m_margin * m_size / 8;
+        m_bit = 7 - (m_margin * m_size % 8);
+        for (int jj = 0; jj < m_qr->width; jj++)
         {
-            for (int kk=0; kk<m_size; kk++)
+            for (int kk = 0; kk < m_size; kk++)
             {
                 *m_q ^= (*m_p & 1) << m_bit;
                 m_bit--;
                 if (m_bit < 0)
                 {
                     m_q++;
-                    m_bit=7;
+                    m_bit = 7;
                 }
             }
             m_p++;
         }
-        for (int kk=0; kk<m_size; kk++)
+        for (int kk = 0; kk < m_size; kk++)
         {
             png_write_row(m_png_ptr, m_row);
         }
@@ -162,7 +225,7 @@ void QR::create_png(const std::string &file_name)
 
 // ------------ GETTERS ----------- //
 std::string QR::to_encode() const { return m_toencode; }
-std::string QR::decoded_data() const { return m_decoded; }
+std::string QR::decoded_data() const { return m_decoded.data; }
 
 // ------------ SETTERS ----------- //
 void QR::set_camera(const int &camera_id) { m_camera = camera_id; }
